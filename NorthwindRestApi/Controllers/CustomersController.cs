449fa00a4a0482c8413fba3436b3e20cccc1123a
Pinja Savenius <pinja.savenius@student.careeria.fi>
2024-09-02 11:47:13 +0300

    Add project files.

---
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        //Alustetaan tietokantayhteys, eli luodaan instanssi luokasta minkä takana on osaavuus käyttää tietokantaa
+        NorthwindOriginalContext db = new NorthwindOriginalContext(); //voisi olla myös pelkkä = new(); ja tarkottaisi samaa asiaa
+        //Hakee kaikki asiakkaat:
+        [HttpGet]
+        public ActionResult GetAllCustomers()
+        {
+            var asiakkaat = db.Customers.ToList();
+            return Ok(asiakkaat);
+        }
+    }
+}
