4bbaef42c44c69780533c0600fe2550e30cd521e
Pinja Savenius <pinja.savenius@student.careeria.fi>
2024-09-03 16:17:54 +0300

    commit

--- NorthwindRestApi/Program.cs
+++ NorthwindRestApi/Program.cs
@@ -16,6 +16,16 @@ builder.Services.AddDbContext<NorthwindOriginalContext>(options => options.UseSq
     // builder.Configuration.GetConnectionString("pilvi")
     ));
 
+// ------------- Cors määritys ------------
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("all",
+    builder => builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("all");
 
 app.UseAuthorization();
 
